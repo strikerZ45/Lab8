dbaadd1db44de955f8cd82da53b0bcec99a44fc3
Allen Domalik <allenDomalik@yahoo.com>
2023-10-30 12:20:02 -0500

    added some lines of java code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function init(){
-//add your javascrip between these two lines of code
- 
-
-
-
-
-
-
+  var button = document.getElementById('entrybutton');
+  
+  function showMeText(){
+	var textbox = document.getElementById('entryinput');
+	document.getElementById('textoutput').innerHTML = textbox.value;
+	alert("Allen Domalik: " + textbox.value);
+  }
+  button.addEventListener('click', showMeText);
+}
 
 window.addEventListener('load', init);
